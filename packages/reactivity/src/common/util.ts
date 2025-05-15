7bcf7b4bb6d91e4746f3fb101424945b5dc5b2c1
ka-jo <baldwinjkaleb@gmail.com>
2025-05-15 14:20:58 -0400

    createObserver util returns stable observer reference given the same first argument

--- packages/reactivity/src/common/util.ts
+++ packages/reactivity/src/common/util.ts
@@ -1,4 +1,5 @@
 import type { Observer } from "@/common/types";
+import { $observer } from "@/common/symbols";
 
 export const NO_OP = function noop() {};
 
@@ -7,19 +8,29 @@ export function createObserver<T>(
 	onError?: Observer<T>["error"],
 	onComplete?: Observer<T>["complete"]
 ): Observer<T> {
+	//@ts-expect-error: we use the symbol to cache the resolved observer on the first argument
+	// itself. This should means given the same function or observer object, we will always
+	// return the same resolved observer. Typescript doesn't like us trying to add a new symbol
+	// that it's unaware of though, so we need to use `@ts-expect-error` here.
+	let observer: Observer<T> = onNextOrObserver[$observer];
+
+	if (observer) return observer;
+
 	if (typeof onNextOrObserver === "function") {
-		return {
+		observer = {
 			next: onNextOrObserver,
 			error: onError || NO_OP,
 			complete: onComplete || NO_OP,
 			dirty: NO_OP,
 		};
 	} else {
-		return {
+		observer = {
 			next: onNextOrObserver.next || NO_OP,
 			error: onNextOrObserver.error || NO_OP,
 			complete: onNextOrObserver.complete || NO_OP,
 			dirty: onNextOrObserver.dirty || NO_OP,
 		};
 	}
+	//@ts-expect-error
+	return (onNextOrObserver[$observer] = observer);
 }
